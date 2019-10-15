ad28575f43a041580fb25040876f0bf54ffee16d
jamekoch117 <jamekoch117@SNS-D-130RZZ>
2019-10-15 13:24:37 -0400

    Add project files.

---
+++ Resterant/Form1.Designer.cs
@@ -0,0 +1,317 @@
+﻿namespace Resterant
+{
+    partial class outputSubtotal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.outputTyrone = new System.Windows.Forms.Label();
+            this.outputQuiche = new System.Windows.Forms.Label();
+            this.outputMacaroni = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.outputBread = new System.Windows.Forms.Label();
+            this.outputCalculate = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.outputTax = new System.Windows.Forms.Label();
+            this.textboxQuiche = new System.Windows.Forms.NumericUpDown();
+            this.textboxMacaroni = new System.Windows.Forms.NumericUpDown();
+            this.textboxGrilled = new System.Windows.Forms.NumericUpDown();
+            this.outputMessage = new System.Windows.Forms.Label();
+            this.outputTotal = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textboxTendered = new System.Windows.Forms.TextBox();
+            this.outputRecipt = new System.Windows.Forms.Button();
+            this.labelTotal = new System.Windows.Forms.Label();
+            this.labelTax = new System.Windows.Forms.Label();
+            this.labelSubtotal = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.outputChange = new System.Windows.Forms.Label();
+            this.outputChange2 = new System.Windows.Forms.Label();
+            this.outputLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.textboxQuiche)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textboxMacaroni)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textboxGrilled)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // outputTyrone
+            // 
+            this.outputTyrone.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.outputTyrone.Location = new System.Drawing.Point(0, -1);
+            this.outputTyrone.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.outputTyrone.Name = "outputTyrone";
+            this.outputTyrone.Size = new System.Drawing.Size(822, 97);
+            this.outputTyrone.TabIndex = 0;
+            this.outputTyrone.Text = "Tryones home cooking";
+            this.outputTyrone.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // outputQuiche
+            // 
+            this.outputQuiche.AutoSize = true;
+            this.outputQuiche.Location = new System.Drawing.Point(12, 127);
+            this.outputQuiche.Name = "outputQuiche";
+            this.outputQuiche.Size = new System.Drawing.Size(128, 25);
+            this.outputQuiche.TabIndex = 1;
+            this.outputQuiche.Text = "Quiche  $12";
+            // 
+            // outputMacaroni
+            // 
+            this.outputMacaroni.AutoSize = true;
+            this.outputMacaroni.Location = new System.Drawing.Point(12, 152);
+            this.outputMacaroni.Name = "outputMacaroni";
+            this.outputMacaroni.Size = new System.Drawing.Size(317, 25);
+            this.outputMacaroni.TabIndex = 2;
+            this.outputMacaroni.Text = "Macaroni without cheese  $6.50";
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.label1.Location = new System.Drawing.Point(318, 96);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(137, 32);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Quantity";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // outputBread
+            // 
+            this.outputBread.AutoSize = true;
+            this.outputBread.Location = new System.Drawing.Point(9, 183);
+            this.outputBread.Name = "outputBread";
+            this.outputBread.Size = new System.Drawing.Size(274, 25);
+            this.outputBread.TabIndex = 4;
+            this.outputBread.Text = "Grilled panera bread  $5.56";
+            // 
+            // outputCalculate
+            // 
+            this.outputCalculate.Location = new System.Drawing.Point(50, 205);
+            this.outputCalculate.Name = "outputCalculate";
+            this.outputCalculate.Size = new System.Drawing.Size(179, 34);
+            this.outputCalculate.TabIndex = 12;
+            this.outputCalculate.Text = "Calculate Cost";
+            this.outputCalculate.UseVisualStyleBackColor = true;
+            this.outputCalculate.Click += new System.EventHandler(this.OutputCalculate_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 264);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 25);
+            this.label2.TabIndex = 13;
+            // 
+            // outputTax
+            // 
+            this.outputTax.AutoSize = true;
+            this.outputTax.Location = new System.Drawing.Point(9, 289);
+            this.outputTax.Name = "outputTax";
+            this.outputTax.Size = new System.Drawing.Size(0, 25);
+            this.outputTax.TabIndex = 14;
+            // 
+            // textboxQuiche
+            // 
+            this.textboxQuiche.Location = new System.Drawing.Point(334, 125);
+            this.textboxQuiche.Name = "textboxQuiche";
+            this.textboxQuiche.Size = new System.Drawing.Size(120, 31);
+            this.textboxQuiche.TabIndex = 20;
+            // 
+            // textboxMacaroni
+            // 
+            this.textboxMacaroni.Location = new System.Drawing.Point(334, 152);
+            this.textboxMacaroni.Name = "textboxMacaroni";
+            this.textboxMacaroni.Size = new System.Drawing.Size(120, 31);
+            this.textboxMacaroni.TabIndex = 21;
+            // 
+            // textboxGrilled
+            // 
+            this.textboxGrilled.Location = new System.Drawing.Point(334, 177);
+            this.textboxGrilled.Name = "textboxGrilled";
+            this.textboxGrilled.Size = new System.Drawing.Size(120, 31);
+            this.textboxGrilled.TabIndex = 22;
+            // 
+            // outputMessage
+            // 
+            this.outputMessage.Location = new System.Drawing.Point(246, 236);
+            this.outputMessage.Name = "outputMessage";
+            this.outputMessage.Size = new System.Drawing.Size(320, 31);
+            this.outputMessage.TabIndex = 23;
+            // 
+            // outputTotal
+            // 
+            this.outputTotal.Location = new System.Drawing.Point(12, 327);
+            this.outputTotal.Name = "outputTotal";
+            this.outputTotal.Size = new System.Drawing.Size(217, 21);
+            this.outputTotal.TabIndex = 26;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(20, 333);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(120, 29);
+            this.label4.TabIndex = 27;
+            this.label4.Text = "Tendered";
+            // 
+            // textboxTendered
+            // 
+            this.textboxTendered.Location = new System.Drawing.Point(140, 331);
+            this.textboxTendered.Name = "textboxTendered";
+            this.textboxTendered.Size = new System.Drawing.Size(100, 31);
+            this.textboxTendered.TabIndex = 28;
+            // 
+            // outputRecipt
+            // 
+            this.outputRecipt.Location = new System.Drawing.Point(107, 365);
+            this.outputRecipt.Name = "outputRecipt";
+            this.outputRecipt.Size = new System.Drawing.Size(176, 37);
+            this.outputRecipt.TabIndex = 29;
+            this.outputRecipt.Text = "Print Recipt ";
+            this.outputRecipt.UseVisualStyleBackColor = true;
+            this.outputRecipt.Click += new System.EventHandler(this.OutputRecipt_Click);
+            // 
+            // labelTotal
+            // 
+            this.labelTotal.Location = new System.Drawing.Point(34, 289);
+            this.labelTotal.Name = "labelTotal";
+            this.labelTotal.Size = new System.Drawing.Size(249, 25);
+            this.labelTotal.TabIndex = 30;
+            // 
+            // labelTax
+            // 
+            this.labelTax.Location = new System.Drawing.Point(34, 267);
+            this.labelTax.Name = "labelTax";
+            this.labelTax.Size = new System.Drawing.Size(249, 22);
+            this.labelTax.TabIndex = 31;
+            // 
+            // labelSubtotal
+            // 
+            this.labelSubtotal.Location = new System.Drawing.Point(34, 242);
+            this.labelSubtotal.Name = "labelSubtotal";
+            this.labelSubtotal.Size = new System.Drawing.Size(249, 25);
+            this.labelSubtotal.TabIndex = 32;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(272, 279);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(147, 35);
+            this.button1.TabIndex = 33;
+            this.button1.Text = "Change back";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // outputChange
+            // 
+            this.outputChange.AutoSize = true;
+            this.outputChange.Location = new System.Drawing.Point(307, 327);
+            this.outputChange.Name = "outputChange";
+            this.outputChange.Size = new System.Drawing.Size(0, 25);
+            this.outputChange.TabIndex = 34;
+            // 
+            // outputChange2
+            // 
+            this.outputChange2.Location = new System.Drawing.Point(267, 317);
+            this.outputChange2.Name = "outputChange2";
+            this.outputChange2.Size = new System.Drawing.Size(255, 35);
+            this.outputChange2.TabIndex = 35;
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.outputLabel.Location = new System.Drawing.Point(519, 96);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(303, 322);
+            this.outputLabel.TabIndex = 36;
+            // 
+            // outputSubtotal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(819, 414);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.outputChange2);
+            this.Controls.Add(this.outputChange);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.labelSubtotal);
+            this.Controls.Add(this.labelTax);
+            this.Controls.Add(this.labelTotal);
+            this.Controls.Add(this.outputRecipt);
+            this.Controls.Add(this.textboxTendered);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.outputTotal);
+            this.Controls.Add(this.outputMessage);
+            this.Controls.Add(this.textboxGrilled);
+            this.Controls.Add(this.textboxMacaroni);
+            this.Controls.Add(this.textboxQuiche);
+            this.Controls.Add(this.outputTax);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.outputCalculate);
+            this.Controls.Add(this.outputBread);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.outputMacaroni);
+            this.Controls.Add(this.outputQuiche);
+            this.Controls.Add(this.outputTyrone);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "outputSubtotal";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.textboxQuiche)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textboxMacaroni)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textboxGrilled)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label outputTyrone;
+        private System.Windows.Forms.Label outputQuiche;
+        private System.Windows.Forms.Label outputMacaroni;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label outputBread;
+        private System.Windows.Forms.Button outputCalculate;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label outputTax;
+        private System.Windows.Forms.NumericUpDown textboxQuiche;
+        private System.Windows.Forms.NumericUpDown textboxMacaroni;
+        private System.Windows.Forms.NumericUpDown textboxGrilled;
+        private System.Windows.Forms.Label outputMessage;
+        private System.Windows.Forms.Label outputTotal;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textboxTendered;
+        private System.Windows.Forms.Button outputRecipt;
+        private System.Windows.Forms.Label labelTotal;
+        private System.Windows.Forms.Label labelTax;
+        private System.Windows.Forms.Label labelSubtotal;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label outputChange;
+        private System.Windows.Forms.Label outputChange2;
+        private System.Windows.Forms.Label outputLabel;
+    }
+}
+
---
+++ Resterant/Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using System.Media;
+
+namespace Resterant
+{
+    public partial class outputSubtotal : Form
+    {
+        const double QUICHE = 12;
+        const double MACARONIWITHOUTCHEESE = 6.50;
+        const double GRILLEDPANERABREAD = 5.56;
+        const double TAX = 0.13;
+        double costOrder;
+        double tax;
+        double costOrderWithTax;
+        double change;
+        double tendered;
+        double costofQuiche;
+        double costofMacaroni;
+        double costofGrilled;
+        double subtotal;
+        int numberofQuiche;
+        int numberofMacaroni;
+        int numberofGrilled;
+        double Tendered;
+        double Total;
+
+
+
+        public outputSubtotal()
+        {
+            InitializeComponent();
+        }
+
+        private void OutputCalculate_Click(object sender, EventArgs e)
+        {
+
+
+            try
+            {
+                //amount of numbers ordered
+                numberofQuiche = Convert.ToInt16(textboxQuiche.Text);
+                numberofMacaroni = Convert.ToInt16(textboxMacaroni.Text);
+                numberofGrilled = Convert.ToInt16(textboxGrilled.Text);
+                //
+
+
+                costOrder = costofQuiche + costofMacaroni + costofGrilled;
+            }
+
+            catch
+            {
+                outputMessage.Text = "please enter valid number without letters or decimals";
+                return;
+
+                //labelSubtotal.Text = numberofQuiche * costofMacaroni * costofGrilled;
+            }
+            costofQuiche = numberofQuiche * QUICHE;
+            costofMacaroni = numberofMacaroni * MACARONIWITHOUTCHEESE;
+            costofGrilled = numberofGrilled * GRILLEDPANERABREAD;
+            subtotal = costofGrilled + costofMacaroni + costofQuiche;
+            tax = subtotal * TAX;
+
+
+            costOrderWithTax = subtotal + tax;
+
+            labelSubtotal.Text = "Subtotal: " + (subtotal.ToString("C"));
+            labelTax.Text = "Tax:        " + (tax.ToString("C"));
+            labelTotal.Text = "Total:      " + (costOrderWithTax.ToString("C"));
+            
+
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {//tnedered money
+            try { Tendered = Convert.ToDouble(textboxTendered.Text); }
+            catch { outputMessage.Text = "     Enter a valid amount"; }
+
+            Total = costOrderWithTax;
+            //money back formula
+            change = Tendered - Total;
+                
+            //channge back
+            outputChange2.Text = "Changeback  " + change.ToString("C");
+        }
+
+        private void OutputRecipt_Click(object sender, EventArgs e)
+        {
+
+            //string for recipt 
+
+            Graphics g = outputLabel.CreateGraphics();
+            Font titleFont = new Font("Arial", 20);
+            Font drawFont = new Font("Arial", 15);
+            Font smallFont = new Font("Arial", 9);
+            SolidBrush blackBrush = new SolidBrush(Color.Black);
+            SoundPlayer player = new SoundPlayer(Properties.Resources.printer);
+            //title 
+            
+
+            player.Play();
+            g.DrawString("Tyrones home cooking ", titleFont, blackBrush, 18, 8);
+            Thread.Sleep(450);
+
+
+            //orders
+           
+
+            player.Play();
+            g.DrawString("\n\nQuiche     " + "x" + numberofQuiche + "   " + costofQuiche.ToString("C"), drawFont, blackBrush, 18, 8);
+            Thread.Sleep(450);
+            player.Play();
+            g.DrawString("\n\n\nMacaroni  " + "x" + numberofMacaroni +  "   " + costofMacaroni.ToString("C"), drawFont, blackBrush, 18, 8);
+            Thread.Sleep(450);
+            player.Play();
+            g.DrawString("\n\n\n\nBread " + "x" + numberofGrilled + "   " + costofGrilled.ToString("C"), drawFont, blackBrush, 18, 8);
+
+            //subtotal tax and total
+            Thread.Sleep(450);
+            player.Play();
+            g.DrawString("\n\n\n\n\n\nSubtotal" + subtotal.ToString("C"), drawFont, blackBrush, 18, 8);
+            Thread.Sleep(450);
+            player.Play();
+            g.DrawString("\n\n\n\n\n\n\nTax" + tax.ToString("C"), drawFont, blackBrush,18 , 8);
+            Thread.Sleep(450);
+            player.Play();
+            g.DrawString("\n\n\n\n\n\n\n\nTotal" + Total.ToString("C"), drawFont, blackBrush, 18, 8);
+            Thread.Sleep(450);
+            player.Play();
+            //cash tendered and change back 
+            player.Play();
+            g.DrawString("\n\n\n\n\n\n\n\n\nTendered" + Tendered.ToString("C"), drawFont, blackBrush, 18, 8);
+            Thread.Sleep(450);
+            player.Play();
+            g.DrawString("\n\n\n\n\n\n\n\n\n\nCash Back" + change.ToString("C"), drawFont, blackBrush, 18, 8);
+            Thread.Sleep(450);
+            player.Play();
+            g.DrawString("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nThankyou for coming to tryones home cooking", smallFont, blackBrush, 18, 8);
+            Thread.Sleep(450);
+            player.Play();
+            g.DrawString("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nCome again or call in", smallFont, blackBrush, 18, 8);
+            //player.Play();
+            Thread.Sleep(450);
+
+            g.DrawString("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n519-275-4444", smallFont, blackBrush, 18, 8);
+        }
+    }
+    }
+
+
+
+
+
